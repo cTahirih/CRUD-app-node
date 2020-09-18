57357a2c5b31e893c52b072409839c359f6f0dba
Tahirih <tahysluana@gmail.com>
2020-09-17 22:23:34 -0500

    add variable prod and dev

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,26 @@
 * puerto
 * */
 process.env.PORT = process.env.PORT || 3000;
+
+/*
+* Enviroment
+* */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*
+* Data base
+*
+* */
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/shoes-store'
+} else {
+  urlDB = 'mongodb://shoes-store-user:cintiajaliri1994@ds139944.mlab.com:39944/shoes-store'
+}
+urlDB = 'mongodb://shoes-store-user:cintiajaliri1994@ds139944.mlab.com:39944/shoes-store'
+
+process.env.URLDB = urlDB;
+
+
+
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(require('./routes/user'));
 
 mongoose.connect(
-  'mongodb://localhost:27017/shoesStore',
+  process.env.URLDB,
   {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
   (error, resp) => {
   if(error) throw error;
