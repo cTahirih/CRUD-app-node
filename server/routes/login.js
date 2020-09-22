f9832daafdaa4bba8f634369121cb31b4a5570c6
Tahirih <tahysluana@gmail.com>
2020-09-21 21:10:50 -0500

    Add method login for users

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,9 +4,55 @@ const User = require('../models/user');
 const app = express();
 
 app.post('/login', (req, resp) => {
-  resp.json(({
-    ok: true
-  }));
+  let body = req.body;
+
+  User.findOne({
+    email: body.email
+  }, (onerror, userDB) => {
+    if (onerror) {
+      return resp.status(500).json({
+        ok: false,
+        error: onerror
+      });
+    }
+
+    if (!userDB) {
+      return resp.status(400).json({
+        ok: false,
+        error: {
+          message: 'Usuario o contraseña incorrecto'
+        }
+      });
+    }
+
+    if (!userDB.status) {
+      return resp.status(400).json({
+        ok: false,
+        error: {
+          message: 'El usuario no está activo'
+        }
+      });
+    }
+
+    if (!bcrypt.compareSync(
+      body.password, userDB.password
+    )) {
+      return resp.status(400).json({
+        ok: false,
+        error: {
+          message: 'Usuario o contraseña incorrecto'
+        }
+      });
+    }
+
+    resp.json(({
+      ok: true,
+      user: userDB,
+      token: '123'
+    }));
+
+  });
+
 });
 
 module.exports = app;
