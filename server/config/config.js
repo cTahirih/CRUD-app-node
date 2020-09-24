4800161b487b710502b4d3a68df8953197577ebd
Tahirih <tahysluana@gmail.com>
2020-09-23 23:00:12 -0500

    modify config fyle to protect psw

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv').config();
+
 /* ******
 * puerto
 * */
@@ -18,18 +20,22 @@ process.env.EXP_TOKEN = 60 * 60 * 24 *30;
 /*
  * SEED auth
  * */
-process.env.SEED = 'dev'
+process.env.SEED = process.env.SEED || process.env.SEEDDEV
 
 /*
 * Data base
 *
 * */
+const userDB = process.env.DBUSER;
+const password = process.env.DBPASS;
+const dbName = process.env.DBNAME;
+let urlDB = '';
 
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/shoes-store'
 } else {
   // urlDB = process.env.MONGO_URI;
-  urlDB = 'mongodb://shoes-store-user:cintiajaliri1994@ds139944.mlab.com:39944/shoes-store';
+  urlDB = `mongodb+srv://${userDB}:${password}@sandbox.tvuhf.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 }
 
 process.env.URLDB = urlDB;
